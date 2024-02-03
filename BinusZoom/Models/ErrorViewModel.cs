df346f4c2a38d642be4e645f482fa5cb866f5f33
Raymond Wangsa Putra <rwp1byte@gmail.com>
2024-02-03 12:20:50 +0800

    first commit lagi

---
+++ BinusZoom/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BinusZoom.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
