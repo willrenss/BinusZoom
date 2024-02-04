5c8f08264df0e900c59fa4a5dd25c3fc9d0a181c
RayWP <rwp1byte@gmail.com>
2024-02-05 00:56:48 +0800

    ensure migration everytime startup

--- BinusZoom/Data/BinusZoomContext.cs
+++ BinusZoom/Data/BinusZoomContext.cs
@@ -7,7 +7,9 @@ public class BinusZoomContext : DbContext
 {
     public BinusZoomContext(DbContextOptions<BinusZoomContext> options)
         : base(options)
-    {
+    { 
+        //ensure migrated
+        Database.EnsureCreated();
     }
 
     public DbSet<Meeting> Meeting { get; set; } = default!;
--- BinusZoom/Program.cs
+++ BinusZoom/Program.cs
@@ -8,19 +8,24 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-
         builder.Services.AddDbContext<BinusZoomContext>(options =>
             options.UseMySQL(builder.Configuration.GetConnectionString("BinusZoomContext") ??
                              throw new InvalidOperationException("Connection string 'BinusZoomContext' not found.")));
-
+        
         // Add services to the container.
         builder.Services.AddControllersWithViews();
 
         var app = builder.Build();
-
+       
         // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
         {
+            // ensure database migrated
+            using (var scope = app.Services.GetRequiredService<BinusZoomContext>())
+            {
+                scope.Database.EnsureCreated();
+            }
+            
             app.UseExceptionHandler("/Home/Error");
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
